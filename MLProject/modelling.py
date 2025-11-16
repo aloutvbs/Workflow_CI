72140857a1f6537f82b1c3004c962f0ed7f2c38b
aloutvbs <alyashandy42@gmail.com>
2025-11-16 09:31:05 +0900

    Fix: add manual log_model for Docker build

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -14,28 +14,32 @@ def load_data():
 
 
 def train_model():
-    # aktifkan autolog (Wajib untuk basic)
+    # Aktifkan autolog (tetap boleh)
     mlflow.sklearn.autolog()
 
     X_train, X_test, y_train, y_test = load_data()
 
-    # buat model
     model = RandomForestRegressor(n_estimators=100, random_state=42)
 
-    # mulai training (MLflow akan merekam otomatis)
     with mlflow.start_run():
+
+        # TRAIN
         model.fit(X_train, y_train.values.ravel())
 
-        # prediksi
+        # PREDICT
         y_pred = model.predict(X_test)
 
-        # hitung metrik
+        # METRICS
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
 
         print("MSE:", mse)
         print("R2 Score:", r2)
 
+        
+        # LOG model secara manual agar ada di artifacts/model
+        mlflow.sklearn.log_model(model, "model")
+        
 
 if __name__ == "__main__":
     train_model()
